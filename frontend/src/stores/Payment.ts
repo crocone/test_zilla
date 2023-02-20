9e90d9618a4d4dad5bc67a4c60ab55177dddb345
shevt <shevtsov@croc.one>
2023-02-20 14:59:44 +0400

    fix: payment status text

--- frontend/src/stores/Payment.ts
+++ frontend/src/stores/Payment.ts
@@ -5,7 +5,13 @@ export const STATUS_CANCELED = 'canceled';
 export const STATUS_REFUND = 'refund';
 
 export const STATUS_LIST = [STATUS_WAIT, STATUS_CANCELED, STATUS_COMPLETE, STATUS_PROCESS, STATUS_REFUND];
-export const STATUS_TEXT: Record<string, string>[] = [{STATUS_WAIT: 'Ожидание'}, {STATUS_CANCELED: 'Отменен'}, {STATUS_COMPLETE: 'Выполнен'}, {STATUS_PROCESS: 'Процессинг'}, {STATUS_REFUND: 'Возврат'}];
+export const STATUS_TEXT: Record<string, string> = {
+        [STATUS_WAIT]: 'Ожидание',
+        [STATUS_CANCELED]: 'Отменен',
+        [STATUS_COMPLETE]: 'Выполнен',
+        [STATUS_PROCESS]: 'Процессинг',
+        [STATUS_REFUND]: 'Возврат'
+    }
 
 export interface Payment {
     id: number
